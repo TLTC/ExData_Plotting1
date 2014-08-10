f155031f74ea533fe11336224184973354b89672
TLTC <guanzt@gmail.com>
2014-08-10 18:41:22 -0400

    cleaned up some extra code

--- plot1.r
+++ plot1.r
@@ -9,12 +9,6 @@ plot1<-function (directory){
     ##Subset the two required dates
     data<-full[full$Date %in% c("1/2/2007","2/2/2007"),]
     
-    ##Convert the factor string Date into date format
-    data$Date=as.Date(data$Date,format="%e/%m/%Y")
-    
-    ##Convert the factor string Time into POSXITlt format
-    data$Time=strptime(data$Time,format="%H:%M:%S")
-    
     ##Set up the graphic device png
     png(filename="plot1.png",width=480,height=480)
     
